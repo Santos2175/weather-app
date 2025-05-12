cb1394f79827a37af731b18f9acfac512a6d340e
Santos2175 <santoshgurung949@gmail.com>
2025-05-12 21:39:08 +0545

    feat: useGeolocation hook added

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import { Button } from '@/components/ui/button';
+import { RefreshCcw } from 'lucide-react';
 
 const Dashboard = () => {
-  return <div>Weather Dashboard</div>;
+  return (
+    <div className='space-y-4'>
+      {/* Favourites Cities */}
+      <div className='flex items-center justify-between'>
+        <h1 className='text-xl font-bold tracking-tight'>My Location</h1>
+        <Button variant={'outline'} size={'icon'}>
+          <RefreshCcw className='h-4 w-4' />
+        </Button>
+      </div>
+      {/* Current and hourly weather */}
+    </div>
+  );
 };
 
 export default Dashboard;
