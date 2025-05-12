dea7ad3c31cbdcc68450079b04de9ea4b55d2919
Santos2175 <santoshgurung949@gmail.com>
2025-05-12 22:24:19 +0545

    feat: alert added for location and corrdinates error

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,8 @@
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { Button } from '@/components/ui/button';
 import WeatherSkeleton from '@/components/WeatherSkeleton';
 import { useGeolocation } from '@/hooks/use-geolocation';
-import { RefreshCcw } from 'lucide-react';
+import { AlertTriangle, MapPin, RefreshCcw } from 'lucide-react';
 
 const Dashboard = () => {
   const {
@@ -20,8 +21,43 @@ const Dashboard = () => {
     }
   };
 
+  // Show skeleton on Loading
   if (locationLoading) {
-    <WeatherSkeleton />;
+    return <WeatherSkeleton />;
+  }
+
+  // Show alert on location error
+  if (locationError) {
+    return (
+      <Alert variant={'destructive'}>
+        <AlertTriangle className='h-4 w-4' />
+        <AlertTitle>Location Error</AlertTitle>
+        <AlertDescription className='flex flex-col gap-6'>
+          <p>{locationError}</p>
+          <Button onClick={getLocation} variant={'outline'} className='w-fit'>
+            <MapPin className='mr-2 h-4 w-4' />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  // Show error if no coordinates
+  if (!coordinates) {
+    return (
+      <Alert variant={'destructive'}>
+        <AlertTitle className='h-4 w-4' />
+        <AlertTitle>Location Required</AlertTitle>
+        <AlertDescription className='flex flex-col gap-6'>
+          <p>Please enable location access to see your local weather.</p>
+          <Button onClick={getLocation} variant={'outline'} className='w-fit'>
+            <MapPin className='mr-2 h-4 w-4' />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
   }
 
   return (
