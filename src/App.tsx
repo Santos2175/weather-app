c961514f0ab5bef9dd582034737a42b0da611bee
Santos2175 <santoshgurung949@gmail.com>
2025-05-12 19:57:20 +0545

    feat: api config and tanstack setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,38 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Layout from '@/components/Layout.tsx';
 import { ThemeProvider } from '@/context/theme-provider';
 import Dashboard from '@/pages/Dashboard';
 import City from '@/pages/City';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+      gcTime: 10 * 60 * 1000,
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-          <Layout>
-            <Routes>
-              <Route path='/' element={<Dashboard />} />
-              <Route path='/city/:cityName' element={<City />} />
-            </Routes>
-          </Layout>
-        </ThemeProvider>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+            <Layout>
+              <Routes>
+                <Route path='/' element={<Dashboard />} />
+                <Route path='/city/:cityName' element={<City />} />
+              </Routes>
+            </Layout>
+          </ThemeProvider>
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   );
 }
